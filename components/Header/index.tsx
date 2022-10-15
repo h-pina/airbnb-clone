11b279ea58c4d567ef295b8f57a911ee80d24e95
Hugo Pina <hugopina.2000@gmail.com>
2022-10-15 09:18:11 -0300

    Continuing header development

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,16 +1,39 @@
 import CategoryIcon from "../CategoryIcon";
 import data from "../../mocks/categories";
 import { FC } from "react";
+import styles from "./style.module.scss";
+import Image from "next/image";
 
+import logo from "../../imgs/logo.png";
+import WorldIcon from "../../ico/worldIcon.js";
+import PersonIcon from "../../ico/personIcon.js";
+import Bars from "../../ico/bars.js";
+
+//TODO: Cara tenho certeza que essa estrutura pode ser melhor, mas de inicio foi o melhor que consegui pensar. Acho que o CSS pode ser mais compacto tbm
+//TODO: Criar um novo componente pra parte das categorias. Isso vai ficar muito grande se for tudo pro Header
 const Header: FC = () => {
   return (
-    <header>
-      <div className="UpperHeader">
-        <div className="logo"></div>
-        <div className="searchBar"></div>
-        <div className="userButtons"></div>
+    <header className={styles.header}>
+      <div className={styles.UpperHeader}>
+        <div className={styles.logoContainer}>
+          <div className={styles.logo}>
+            <Image src={logo} alt="logo" />
+          </div>
+        </div>
+
+        <div className={styles.searchBarContainer}>
+          <input className={styles.searchBar} type="text" />
+        </div>
+        <div className={styles.userButtonsContainer}>
+          <div className={styles.userButtons}>
+            <WorldIcon />
+            <PersonIcon />
+            <Bars />
+          </div>
+        </div>
       </div>
-      <div className="LowerHeader">
+
+      {/*<div className="LowerHeader">
         {data.map((item) => {
           return (
             <>
@@ -18,7 +41,7 @@ const Header: FC = () => {
             </>
           );
         })}
-      </div>
+      </div>*/}
     </header>
   );
 };
