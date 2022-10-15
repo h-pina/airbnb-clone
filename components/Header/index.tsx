978e51bd7432d8db603cb45fcb0ae008833371db
Hugo Pina <hugopina.2000@gmail.com>
2022-10-15 15:18:58 -0300

    fiz coisa pa carai

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,32 +3,66 @@ import data from "../../mocks/categories";
 import { FC } from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-import logo from "../../imgs/logo.png";
-import WorldIcon from "../../ico/worldIcon.js";
-import PersonIcon from "../../ico/personIcon.js";
-import Bars from "../../ico/bars.js";
+//Icons and Images imports
+import FullLogo from "../../svg/airbnbFullLogo.js";
+import ReducedLogo from "../../svg/airbnbReducedLogo.js";
+import WorldIcon from "../../svg/worldIcon.js";
+import PersonIcon from "../../svg/personIcon.js";
+import Bars from "../../svg/bars.js";
 
 //TODO: Cara tenho certeza que essa estrutura pode ser melhor, mas de inicio foi o melhor que consegui pensar. Acho que o CSS pode ser mais compacto tbm
 //TODO: Criar um novo componente pra parte das categorias. Isso vai ficar muito grande se for tudo pro Header
 const Header: FC = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize); //??
+    };
+  }, []);
   return (
     <header className={styles.header}>
-      <div className={styles.UpperHeader}>
+      <div className={styles.logoSection}>
         <div className={styles.logoContainer}>
-          <div className={styles.logo}>
-            <Image src={logo} alt="logo" />
-          </div>
+          {windowWidth > 1130 ? <FullLogo /> : <ReducedLogo />}
         </div>
+      </div>
 
-        <div className={styles.searchBarContainer}>
-          <input className={styles.searchBar} type="text" />
-        </div>
-        <div className={styles.userButtonsContainer}>
-          <div className={styles.userButtons}>
+      <div className={styles.searchBarSection}>
+        <input className={styles.searchBar} type="text" />
+      </div>
+      <div className={styles.userButtonsSection}>
+        <div className={styles.userButtons}>
+          <div className={styles.worldIconButton}>
             <WorldIcon />
-            <PersonIcon />
-            <Bars />
+          </div>
+          <div className={styles.userOptionsContainer}>
+            <div className={styles.userOptions}>
+              <div style={{ width: `25px` }}>
+                <Bars />
+              </div>
+              <div
+                style={{
+                  width: `25px`,
+                  paddingInline: `2px`,
+                  paddingTop: "1px",
+                  color: "white",
+                  backgroundColor: "grey",
+                  borderRadius: `320px`,
+                  marginLeft: "10px",
+                }}
+              >
+                <PersonIcon />
+              </div>
+            </div>
           </div>
         </div>
       </div>
