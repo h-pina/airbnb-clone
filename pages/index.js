495050cb1c9e481e23c2facc0d4560a300875b22
Ana Luiza <analuizaalvesm@gmail.com>
2022-10-16 19:16:32 -0300

    branch recriada

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import CategoryHeader from "../components/CategoryHeader";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <CategoryHeader />
       <div style={{ height: "10000px" }} />
     </>
   );
